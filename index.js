d8993d9e88fbba950de5d3977cc3544ccce0e450
Tom de Visser <tom.d6r@outlook.com>
2024-01-14 20:41:19 +0100

    Added a smiley :)

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const http = require('http');
-const fs = require('fs');
+const http = require("http");
+const fs = require("fs");
 
-http.createServer((req, res) => {
-	res.write("Hello from Node");
-	res.end();
-}).listen(3000);
+http
+  .createServer((req, res) => {
+    res.write("Hello from Node :)");
+    res.end();
+  })
+  .listen(3000);
 
-console.log('Server started listening on port 3000');
+console.log("Server started listening on port 3000");
