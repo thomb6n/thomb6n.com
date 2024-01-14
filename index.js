41f48263528a37a334437213353850aa0ac26f47
Tom de Visser <tom.d6r@outlook.com>
2024-01-14 21:57:20 +0100

    Tweaked the server response

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const http = require("http");
 const fs = require("fs");
 
-http
-  .createServer((req, res) => {
-    res.write("Hello from Node :)");
-    res.end();
-  })
-  .listen(3000);
+const server = http.createServer();
+
+server.on("request", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write("<h1>Hello from Node :)</h1>");
+  res.end();
+});
+
+server.listen(3000);
 
 console.log("Server started listening on port 3000");
